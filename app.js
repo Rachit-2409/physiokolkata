6eebc852891288c2197a6389b644df69c9d33913
Rachit-2409 <rachitgupta815@gmail.com>
2025-02-24 20:30:25 +0530

    New UI Update

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ let pointer = document.querySelector('.cursor');
 window.addEventListener('mousemove',(event)=>{
     let Xpos =  event.clientX;
     let Ypos = event.clientY;
-
-    pointer.style.top = Ypos+'px';
-    pointer.style.left = Xpos+'px';
+    // pointer.style.top = Ypos+'px';
+    // pointer.style.left = Xpos+'px';
+   
+  pointer.animate(
+    {
+        left : `${Xpos}px`,
+        top : `${Ypos}px`
+    }
+    , {
+        duration : 1000,
+        fill: "forwards"
+    }
+  )
    
     console.log(Xpos,Ypos)
 })
